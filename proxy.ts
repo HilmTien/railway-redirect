a133772d85b37b4232919c02059667b28422b6f2
HilmTien <tient2477@gmail.com>
2025-11-10 12:34:28 +0100

    feat: redirect everything to railway

---
+++ proxy.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  // The original path and query
+  const { pathname, search } = request.nextUrl;
+
+  // Define your target hostname
+  const targetHost = "it1901-itp-production.up.railway.app";
+
+  // Construct new URL
+  const redirectUrl = new URL(`https://${targetHost}${pathname}${search}`);
+
+  // Return a redirect response
+  return NextResponse.redirect(redirectUrl);
+}
+
+// Optionally limit which paths the middleware runs on
+export const config = {
+  matcher: "/:path*",
+};
